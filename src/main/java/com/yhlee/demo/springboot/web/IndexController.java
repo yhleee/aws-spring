ef2c1026a1f68f59ac488343d3b4a22d6382549d
yunhee.lee <lee.yunheee@gmail.com>
2021-03-25 00:59:52 +0900

    로그인 기능 추가 (구글)

--- src/main/java/com/yhlee/demo/springboot/web/IndexController.java
+++ src/main/java/com/yhlee/demo/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.yhlee.demo.springboot.web;
 
+import com.yhlee.demo.springboot.config.auth.LoginUser;
+import com.yhlee.demo.springboot.config.auth.dto.SessionUser;
 import com.yhlee.demo.springboot.service.PostsService;
 import com.yhlee.demo.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -14,8 +16,11 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
